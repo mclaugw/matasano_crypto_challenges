106273bb504d53bd315b0bde99dea9c385b42928
mclaugw <mclaugw@users.noreply.github.com>
2014-12-17 01:03:51 -0500

    created function

--- 03_single_char_xor_cipher.py
+++ 03_single_char_xor_cipher.py
@@ -2,21 +2,27 @@ import base64
 import string
 import charfreq
 from collections import Counter
+from pprint import pprint
 
-data = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-dlen = len(data) / 2
-data = int(data, 16)
+d = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 chars_by_result = {}   # dict with xor result as key and key as value (lol)
 scores_by_result = {}  # dict with xor result as key and score as value
 
-for c in string.printable:
-    cstr = int(base64.b16encode(c * dlen), 16)
-    xor_res = base64.b16decode(hex(data ^ cstr)[2:-1], True)
-    chars_by_result[xor_res] = c
+def calc_xor(data):
+    dlen = len(data) / 2
+    data = int(data, 16)
+    for c in string.printable:
+        cstr = int(base64.b16encode(c * dlen), 16)
+        xor_res = hex(data ^ cstr)[2:-1]
+        while len(xor_res) % 2 != 0:
+            xor_res = xor_res + '0'
+        xor_res = base64.b16decode(xor_res, True)
+        chars_by_result[xor_res] = c
 
-    if(all(d in string.printable for d in xor_res)):
-        scores_by_result[xor_res] = charfreq.cf_eval(xor_res)
+        if(all(d in string.printable for d in xor_res)):
+            scores_by_result[xor_res] = charfreq.cf_eval(xor_res)
 
+calc_xor(d)
 top3 = Counter(scores_by_result).most_common(3)
 print "most likely keys: "
 for i in range(3):
