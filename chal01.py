48a9f9a94d1bb313e41731fd03277e136d077616
mclaugw <mclaugw@users.noreply.github.com>
2014-12-24 00:54:59 -0500

    refactored chal01

--- chal01.py
+++ chal01.py
@@ -5,12 +5,9 @@ import base64
 
 hex_data = '49276d206b696c6c696e6720796f757220627261696e206c' + \
            '696b65206120706f69736f6e6f7573206d757368726f6f6d'
-hex_data = hex_data.upper()
+known_result = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBs' + \
+               'aWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
-print 'hex: ' + hex_data
-hex_data = base64.b16decode(hex_data)
-hex_data = base64.b64encode(hex_data)
-print 'b64: ' + hex_data
-hex_data = base64.b64decode(hex_data)
-hex_data = base64.b16encode(hex_data)
-print 'hex: ' + hex_data
+hex_data = base64.b64encode(base64.b16decode(hex_data, True))
+
+assert hex_data == known_result
