1277f7fc838627c2410d317445235f6e760eb7ec
mclaugw <mclaugw@users.noreply.github.com>
2014-12-24 00:55:09 -0500

    refactored chal02.py

--- chal02.py
+++ chal02.py
@@ -2,8 +2,10 @@
 
 one = "1c0111001f010100061a024b53535009181c"
 two = "686974207468652062756c6c277320657965"
+known_result = "746865206b696420646f6e277420706c6179"
 
 one = int(one, 16)
 two = int(two, 16)
-res = hex(one ^ two)
-print res[2:-1]
+res = hex(one ^ two)[2:-1]
+
+assert res == known_result
